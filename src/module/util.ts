22961dc5a964e64d85ec29463796a61a42158633
闫蒙 <yanmeng@hujiang.com>
2017-08-21 17:45:40 +0800

    test git rebase

--- src/module/util.ts
+++ src/module/util.ts
@@ -1,19 +1,21 @@
 /**
  * Created by yanmeng on 2017/8/5.
  */
-import * as $Promise from 'bluebird';
 
-export default function PromiseFunc(url: string): any {
-  return new $Promise<string>((resolve: any, reject: any) => {
+enum methodType {'GET','PUT','PATCH','DELETE','POST'};
+
+export default function(method: string, url: string, data: any): any {
+  return new Promise<string>((resolve: Function, reject: Function) => {
     let xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
+
+    xhr.open(method, url, true); // true 代表是否异步发送
     xhr.onreadystatechange = handler;
     xhr.responseType = "json";
-    xhr.setRequestHeader("Accept", "application/json");
-    xhr.send();
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(data);
 
     function handler() {
-      if (this.status === 200) {
+      if(xhr.readyState == 4 && xhr.status === 200) {
         resolve(this.response);
       } else {
         reject(new Error(this.statusText));
